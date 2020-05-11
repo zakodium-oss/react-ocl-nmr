87fc0d1e4b7cf26a6208b99c0e1081c4bf5efd6e
Luc Patiny <luc@patiny.com>
2020-05-11 17:59:02 +0200

    chore: export OCLnmr as default

--- src/components/OCLnmr.js
+++ src/components/OCLnmr.js
@@ -9,7 +9,7 @@ import { MolfileSvgRenderer } from 'react-ocl';
 
 initOCL(OCL);
 
-export function OCLnmr(props) {
+export default function OCLnmr(props) {
   const { molfile, setMolfile, setSelectedAtom, highlights } = props;
   const [overHighlights, setOverHighlights] = useState([]);
   const [externalHighlights, setExternalHighlights] = useState([]);
